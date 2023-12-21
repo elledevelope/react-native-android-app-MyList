f046625f610f6ce1f06cb6f3d5b9229734548f12
Elmira PETIT <elledevelope@gmail.com>
2023-12-21 14:20:41 +0100

    Second way to render Result under Champ in FlatList

--- components/Result/Result.jsx
+++ components/Result/Result.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
 import TodoListContext from '../../TodoListContext.js';
 
 const Result = () => {
@@ -7,14 +7,22 @@ const Result = () => {
   const todoListContext = React.useContext(TodoListContext);
 
   return (
-    <View>
-      {/* Using map to iterate over 'list' array */}
-      {todoListContext.list.map((value, index) => (
-        // Rendering a Text component for each item in the 'list' array
-        // 'key' is set to the index for React to efficiently track and manage the list items
-        <Text key={index}>{value}</Text>
-      ))}
-    </View>
+
+    <FlatList
+      data={todoListContext.list}
+      renderItem={({ item }) => <View><Text>{item}</Text></View>}
+      keyExtractor={item => item}
+    >
+    </FlatList>
+
+    // <View>
+    //   {/* Using map to iterate over 'list' array */}
+    //   {todoListContext.list.map((value, index) => (
+    //     // Rendering a Text component for each item in the 'list' array
+    //     // 'key' is set to the index for React to efficiently track and manage the list items
+    //     <Text key={index}>{value}</Text>
+    //   ))}
+    // </View>
   );
 };
 
