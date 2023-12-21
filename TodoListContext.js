228079251a2b1b77c5766c58df6ac4bdd371bdb2
Elmira PETIT <elledevelope@gmail.com>
2023-12-21 16:03:45 +0100

    Update

--- TodoListContext.js
+++ TodoListContext.js
@@ -1,5 +1,21 @@
 import  React from "react";
-
 const TodoListContext = React.createContext();
-
 export default TodoListContext;
+
+
+// import React, { createContext } from 'react';
+// import { MMKV } from 'react-native-mmkv';
+
+// export const TodoListContext = createContext();
+
+// export const TodoListProvider = ({ children }) => {
+//   const storage = new MMKV();
+
+//   // Your context state and logic go here
+
+//   return (
+//     <TodoListContext.Provider value={{ storage }}>
+//       {children}
+//     </TodoListContext.Provider>
+//   );
+// };
