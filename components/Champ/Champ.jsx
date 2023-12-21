03b194de6cdfaaadbdc1403ebc7cf51c79bf4c14
Elmira PETIT <elledevelope@gmail.com>
2023-12-21 13:47:31 +0100

    Added onChangeText and value in Champ.jsx

--- components/Champ/Champ.jsx
+++ components/Champ/Champ.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { TextInput } from 'react-native'
 import { styleChamp } from "./styleChamp.js";
+import TodoListContext from '../../TodoListContext.js';
 
 const Champ = () => {
-  return (
-    <TextInput 
-    placeholder="Nouvel Item"
-    style={styleChamp} 
-    >
+  const todoListContext = React.useContext(TodoListContext);
 
+  return (
+    <TextInput
+      placeholder="Nouvel Item"
+      style={styleChamp}
+      onChangeText={(text) => { todoListContext.setEnter(text) }}
+    >
     </TextInput>
   )
 }
