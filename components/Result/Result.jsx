2164e0229002f30cb30118a19c0bd12dfcb64dac
Elmira PETIT <elledevelope@gmail.com>
2023-12-21 14:15:32 +0100

    Using map to iterate over 'list' array in Result

--- components/Result/Result.jsx
+++ components/Result/Result.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import { FlatList } from 'react-native'
+import React from 'react';
+import { View, Text } from 'react-native';
+import TodoListContext from '../../TodoListContext.js';
 
 const Result = () => {
-  return (
-    <FlatList></FlatList>
-  )
-}
+  // Accessing the TodoListContext using the useContext hook from the React library
+  const todoListContext = React.useContext(TodoListContext);
 
-export default Result
+  return (
+    <View>
+      {/* Using map to iterate over 'list' array */}
+      {todoListContext.list.map((value, index) => (
+        // Rendering a Text component for each item in the 'list' array
+        // 'key' is set to the index for React to efficiently track and manage the list items
+        <Text key={index}>{value}</Text>
+      ))}
+    </View>
+  );
+};
+
+export default Result;
