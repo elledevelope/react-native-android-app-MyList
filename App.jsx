383ec238352d9c0388fb2b3c63e05bf9885f1510
Elmira PETIT <elledevelope@gmail.com>
2023-12-21 16:03:27 +0100

    Added MMKV storage

--- App.jsx
+++ App.jsx
@@ -5,7 +5,7 @@
  * @format
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   Button,
   SafeAreaView,
@@ -20,11 +20,24 @@ import Title from './components/Title/Title';
 import Champ from './components/Champ/Champ';
 import Result from './components/Result/Result';
 import TodoListContext from './TodoListContext';
+import { MMKVLoader, useMMKVStorage } from 'react-native-mmkv-storage';
+const storage = new MMKVLoader().initialize();
 
 function App() {
   // Using the React.useState hook to create a state variable 'list' with an initial value of an empty array and a function 'setList' to update it
   const [list, setList] = React.useState([]);
+  // Using the 'useMMKVStorage' hook to create a state variable 'listStorage'
+  // The state variable is initialized with the value retrieved from the 'storage' instance using the key 'listStorage'
+  // The 'setlistStorage' function is provided to update the 'listStorage' state variable
+  const [listStorage, setlistStorage] = useMMKVStorage('listStorage', storage, list);
   const [enter, setEnter] = React.useState("");
+  useEffect(() => {
+    // Perform actions when 'listStorage' changes
+    if (listStorage) {
+      console.dir(listStorage);
+    }
+  }, [listStorage])
+
 
   // Defining a function 'handlePress' that will be executed when a certain event (e.g., button press) occurs
   const handlePress = () => {
@@ -34,6 +47,7 @@ function App() {
       setList([...list, enter]);
       // Resetting the 'enter' state variable to an empty string, effectively clearing the text input
       setEnter("");
+      //vider le Champ??
 
 
     }
@@ -64,6 +78,7 @@ function App() {
           {/* <Text>{enter}</Text> */}
           {/* <Text>{list}</Text> */}
         </TodoListContext.Provider>
+        <Text>{listStorage}</Text>
       </ScrollView>
     </SafeAreaView >
   );
