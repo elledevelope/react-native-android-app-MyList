ad4c189b6db31cda4910822d882710472aef098f
Elmira PETIT <elledevelope@gmail.com>
2023-12-21 13:48:31 +0100

    Added onPress

--- App.jsx
+++ App.jsx
@@ -19,25 +19,49 @@ import {
 import Title from './components/Title/Title';
 import Champ from './components/Champ/Champ';
 import Result from './components/Result/Result';
+import TodoListContext from './TodoListContext';
 
 function App() {
+  // Using the React.useState hook to create a state variable 'list' with an initial value of an empty array and a function 'setList' to update it
   const [list, setList] = React.useState([]);
+  const [enter, setEnter] = React.useState("");
+
+  // Defining a function 'handlePress' that will be executed when a certain event (e.g., button press) occurs
+  const handlePress = () => {
+    // Checking if the 'enter' variable (text input) has a length greater than 0 (i.e., it's not empty)
+    if (enter.length > 0) {
+      // Updating the 'list' state by creating a new array that includes the current items in 'list' and the new value of 'enter'
+      setList([...list, enter]);
+      // Resetting the 'enter' state variable to an empty string, effectively clearing the text input
+      setEnter("");
+
+
+    }
+  };
 
   return (
     <SafeAreaView>
       <ScrollView
         contentInsetAdjustmentBehavior="automatic">
-        <View>
-          <Title></Title>
-          <Text>Hello world</Text>
-          <Champ></Champ>
-          <Button
-            style={styles.buttonStyle}
-            title='Ajouter'
-            color="salmon"
-          ></Button>
-        </View>
-        <Result></Result>
+        <TodoListContext.Provider value={{ list, setList, enter, setEnter }}>
+          <View>
+            <Title></Title>
+            <Text
+              style={styles.TaskStyle}
+            >Enter Item</Text>
+            <Champ></Champ>
+            <Button
+              style={styles.buttonStyle}
+              title='Ajouter'
+              color="salmon"
+              onPress={handlePress}
+            ></Button>
+          </View>
+          <Result></Result>
+          {/* Displaying the entered text from the enter state: */}
+          {/* <Text>{enter}</Text> */}
+          {/* <Text>{list}</Text> */}
+        </TodoListContext.Provider>
       </ScrollView>
     </SafeAreaView >
   );
@@ -47,6 +71,10 @@ const styles = StyleSheet.create({
   buttonStyle: {
     color: 'black'
   },
+  TaskStyle: {
+    textAlign: 'center',
+    fontSize: 20
+  }
 });
 
 export default App;
--- components/Champ/Champ.jsx
+++ components/Champ/Champ.jsx
@@ -10,7 +10,14 @@ const Champ = () => {
     <TextInput
       placeholder="Nouvel Item"
       style={styleChamp}
+      // Setting up the onChangeText prop to call a function when the text in the input changes
+      // The function receives the updated text as 'text' and uses 'todoListContext.setEnter' to update the 'enter' state in the context with the new text
       onChangeText={(text) => { todoListContext.setEnter(text) }}
+
+      // Setting the value prop to control the input's value, using the 'enter' state from the context
+      // This ensures that the input reflects the current value of the 'enter' state in the context
+      value={todoListContext.enter}
+
     >
     </TextInput>
   )
